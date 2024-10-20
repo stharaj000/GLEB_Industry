ed1b99000dd07656cef60e4c0f9ca9145c1595ce
stharaj000 <sthagaming17@gmail.com>
2024-10-20 16:13:56 +0545

    responsivenes:)

---
+++ script.js
@@ -0,0 +1,17 @@
+function mediaFunction(){
+    document.getElementsByClassName("afterMedia")[0].style.display="block";
+    document.getElementsByClassName("hamburger")[0].style.display="none";
+    document.getElementsByClassName("x")[0].style.display="block";
+}
+
+function RmediaFunction(){
+    document.getElementsByClassName("afterMedia")[0].style.display="none";
+    document.getElementsByClassName("hamburger")[0].style.display="block";
+    document.getElementsByClassName("x")[0].style.display="none";
+}
+
+
+
+
+
+
